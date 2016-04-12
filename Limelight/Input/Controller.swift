790ccb79520e334ec20ea86dafc502b00cd52823
David Aghassi <david.aghassi@gmail.com>
2016-04-11 20:45:50 -0400

    Added comments, created MoonlightUnitTest
    -  Added comments for Controller.swift
    -  Created MoonlightUnitTest for testing new class

--- Limelight/Input/Controller.swift
+++ Limelight/Input/Controller.swift
@@ -9,9 +9,18 @@
 import Foundation
 
 @objc
+/**
+ Defines a controller layout
+ */
 class Controller: NSObject {
   // Swift requires initial properties
-  var playerIndex: CInt = 0, lastButtonFlags: CInt = 0, emulatingButtonFlags: CInt = 0
-  var lastLeftTrigger: CChar = 0, lastRightTrigger: CChar = 0
-  var lastLeftStickX: CShort = 0, lastLeftStickY: CShort = 0, lastRightStickX: CShort = 0, lastRightStickY: CShort = 0
+  var playerIndex: CInt = 0           // Controller number (e.g. 1, 2 ,3 etc)
+  var lastButtonFlags: CInt = 0
+  var emulatingButtonFlags: CInt = 0
+  var lastLeftTrigger: CChar = 0      // Last left trigger pressed
+  var lastRightTrigger: CChar = 0     // Last right trigger pressed
+  var lastLeftStickX: CShort = 0      // Last X direction the left joystick went
+  var lastLeftStickY: CShort = 0      // Last Y direction the left joystick went
+  var lastRightStickX: CShort = 0     // Last X direction the right joystick went
+  var lastRightStickY: CShort = 0     // Last Y direction the right joystick went
 }
