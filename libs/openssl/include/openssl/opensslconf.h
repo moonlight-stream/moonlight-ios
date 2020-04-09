5264fee65511928758aa0103afd7de4ae5b3a1f0
Cameron Gutman <aicommander@gmail.com>
2020-04-08 19:57:45 -0700

    Update to OpenSSL 1.1.1f
    Built for targets: ios-sim-cross-i386 ios-sim-cross-x86_64 ios64-cross-arm64 ios64-cross-arm64e ios-cross-armv7s ios-cross-armv7 tvos-sim-cross-x86_64 tvos64-cross-arm64
    
    https://github.com/keeshux/openssl-apple

--- libs/openssl/include/openssl/opensslconf.h
+++ libs/openssl/include/openssl/opensslconf.h
@@ -8,7 +8,9 @@
 
 #include <TargetConditionals.h>
 
-#if TARGET_OS_IOS && TARGET_OS_SIMULATOR && TARGET_CPU_X86_64
+#if TARGET_OS_IOS && TARGET_OS_SIMULATOR && TARGET_CPU_X86
+# include <openssl/opensslconf_ios_i386.h>
+#elif TARGET_OS_IOS && TARGET_OS_SIMULATOR && TARGET_CPU_X86_64
 # include <openssl/opensslconf_ios_x86_64.h>
 #elif TARGET_OS_IOS && TARGET_OS_EMBEDDED && TARGET_CPU_ARM64
 # include <openssl/opensslconf_ios_arm64.h>
