400d0a9ffd02c5d1af33303d7d17a86abcf6f395
Long Le <longkimle@hotmail.com>
2022-11-25 16:30:10 -0800

    added files for OSC profiles

---
+++ Limelight/Input/OSCProfile.m
@@ -0,0 +1,15 @@
+//
+//  OSCProfile.m
+//  Moonlight
+//
+//  Created by Long Le on 11/25/22.
+//  Copyright © 2022 Moonlight Game Streaming Project. All rights reserved.
+//
+
+#import "OSCProfile.h"
+
+@implementation OSCProfile
+
+@synthesize name;
+
+@end
---
+++ Limelight/ViewControllers/OSCProfilesTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  OSCProfilesTableViewController.h
+//  Moonlight
+//
+//  Created by Long Le on 11/25/22.
+//  Copyright © 2022 Moonlight Game Streaming Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OSCProfilesTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
